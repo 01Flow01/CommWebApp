4c6c10e1b505c5447563cc5d61af1e480fc23b75
01Flow01 <01Flow01@users.noreply.github.com>
2020-11-16 10:28:45 +0000

    WebCommApp first check-in

---
+++ CommWebApp/src/main/java/com/commwebapp/CommWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.commwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommWebAppApplication.class, args);
+	}
+
+}
---
+++ CommWebApp/src/test/java/com/commwebapp/CommWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
